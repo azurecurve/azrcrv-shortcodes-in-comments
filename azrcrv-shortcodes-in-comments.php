c4acc7752fb5814a25890c5881ae71a2d3e3603f
azurecurve <github@azurecurve.co.uk>
2024-02-03 15:19:26 +0000

    Release v1.2.5

--- azrcrv-shortcodes-in-comments.php
+++ azrcrv-shortcodes-in-comments.php
@@ -1,14 +1,18 @@
 <?php
 /**
  * ------------------------------------------------------------------------------
- * Plugin Name: Shortcodes in Comments
- * Description: Allows shortcodes to be used in comments
- * Version: 1.2.4
- * Author: azurecurve
- * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
- * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/shortcodes-in-comments/
- * Text Domain: shortcodes-in-comments
- * Domain Path: /languages
+ * Plugin Name:		Shortcodes in Comments
+ * Description:		Allows shortcodes to be used in comments
+ * Version:			1.2.5
+ * Requires CP:		1.0
+ * Author:			azurecurve
+ * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
+ * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/shortcodes-in-comments/
+ * Donate link:		https://development.azurecurve.co.uk/support-development/
+ * Text Domain:		shortcodes-in-comments
+ * Domain Path:		/languages
+ * License:			GPLv2 or later
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.html
  * ------------------------------------------------------------------------------
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
